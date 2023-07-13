1998243d13fc504b6d9d8f485a35b4aace87d145
WindJammer6 <98175995+WindJammer6@users.noreply.github.com>
2023-07-13 23:58:09 +0800

    Update readme_generator.py

--- readme_generator.py
+++ readme_generator.py
@@ -11,7 +11,7 @@ print(start_of_readme)
 file_types_dictionary = {'C++': 'cpp', 'Python': 'py', 'Haskell': 'hs', 'C': 'c'}
 
 #Getting the Leetcode Solutions
-stream = os.popen('find | grep kattis')
+stream = os.popen('find | grep leetcode')
 lines = stream.readlines()
 
 things_to_write = []
@@ -19,7 +19,7 @@ for line in lines:
     line = line.strip()
 
     # Matches kattis_xxxx.py or kattis_xxxx.cpp or kattis_xxxx.hs
-    m = re.search(r'\/kattis_(\w*)\.(?:py|cpp|hs|c)', line)
+    m = re.search(r'\/leetcode_(\w*)\.(?:py|cpp|hs|c)', line)
 
     things_to_write.append([str(m.group(1)), "https://github.com/WindJammer6/14.-My-Leetcode-Solutions/blob/main" +
                             line[1:], "https://leetcode.com/problems/" + str(m.group(1))])
