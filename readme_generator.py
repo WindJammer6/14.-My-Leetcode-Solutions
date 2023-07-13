0499a3aac0ddf19e9a3365cd7966f8e370bae588
WindJammer6 <98175995+WindJammer6@users.noreply.github.com>
2023-07-14 00:36:55 +0800

    Update readme_generator.py

--- readme_generator.py
+++ readme_generator.py
@@ -1,10 +1,19 @@
 import os, re
 
-start_of_readme = '''
-# List of LeetCode Questions Solved
+start_of_readme = '''# 14.-My-Leetcode-Solutions :dart:
+Here are some of my accepted attempts at some Leetcode questions. And hey, I managed to learn how to automate the process of updating my README.md file whenever I solved and 
+upload a new solution to a Leetcode question in this repository!
+
+My Leetcode account: https://leetcode.com/WindJammer6/
+
+<ins>Disclaimer</ins>: I am a student programmer, these solutions that I have attached in this repository are not perfect answers to the Leetcode questions (in terms of design 
+and Time and Space Big O Notation Complexity), but are just merely my sharing of my own approach to the questions (in terms of design and Time and Space Big O Notation Complexity).
+They do answer the Leetcode questions, but there are definitely better answer codes out there.
+
+# List of Leetcode Questions Solved
 ![Auto Update](https://github.com/WindJammer6/14.-My-Leetcode-Solutions/actions/workflows/update_readme.yml/badge.svg)
-| Index | Question Title | Solution |
-| ----- | -------------- | -------- |'''
+| Leetcode Question Index | Leetcode Question Title | Solution and Programming Language Used |
+| ----------------------- | ----------------------- | -------------------------------------- |'''
 
 print(start_of_readme)
 
