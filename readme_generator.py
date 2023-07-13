5d0a2f77393236a7a36124255856872a239ec681
WindJammer6 <98175995+WindJammer6@users.noreply.github.com>
2023-07-13 22:33:55 +0800

    Update readme_generator.py

--- readme_generator.py
+++ readme_generator.py
@@ -1,25 +1,6 @@
 import os, re
 
-start_of_readme = '''# Competitive Programming 4 Solutions
-My solutions to some of the [Kattis](https://open.kattis.com/) problems listed in [Competitive Programming 4](https://cpbook.net/details?cp=4).
-Some [UVA Online Judge](https://onlinejudge.org/) problems are also solved on [Virtual Judge](https://vjudge.net/).
-
-The C++ code is written to be compiled using with gcc version 11.2.0 (Ubuntu 11.2.0-19ubuntu1)
-<pre>g++ -std=c++17 kattis_xxx.cpp -o kattis_xxx && kattis_xxx</pre>
-
-Python code is executed using Python 3.10.4 on my machine but will work on the 3.6 of Kattis
-<pre>/bin/python3 kattis_xxx.py</pre>
-
-## Documentation
-| Category | Description |
-|----------|-------------|
-| Observation | A key insight into the question or our currently considered method that might be needed to come up with a correct algorithm or improve the efficiency of an algorithm. |
-| Idea | A new approach to the problem. Could include viewing the problem from a different angle. |
-| Debugging | Descriptions of bugs that one could encounter, possibly including edge cases. |
-| Optimisations | Other micro-optimisations that are not important enough to be considered ideas or observations. |
-| Design | A way we implement an idea in code to keep the code simple and easy to write. |
-| Time & Space Complexity | My analysis of the algorithmic order of growth |
-
+start_of_readme = '''
 # List of Kattis Questions Solved
 ![Auto Update](https://github.com/BrandonTang89/Competitive_Programming_4_Solutions/actions/workflows/Update_README.yml/badge.svg)
 | Index | Question Title | Solution |
@@ -30,7 +11,7 @@ print(start_of_readme)
 file_types_dictionary = {'C++': 'cpp', 'Python': 'py', 'Haskell': 'hs', 'C': 'c'}
 
 #Getting the Leetcode Solutions
-stream = os.popen('find | grep kattis')
+stream = os.popen('find | grep Leetcode')
 lines = stream.readlines()
 
 things_to_write = []
@@ -38,7 +19,7 @@ for line in lines:
     line = line.strip()
 
     # Matches kattis_xxxx.py or kattis_xxxx.cpp or kattis_xxxx.hs
-    m = re.search(r'\/kattis_(\w*)\.(?:py|cpp|hs|c)', line)
+    m = re.search(r'\/Leetcode_(\w*)\.(?:py|cpp|hs|c)', line)
 
     things_to_write.append([str(m.group(1)), "https://github.com/WindJammer6/14.-My-Leetcode-Solutions/blob/main" +
                             line[1:], "https://leetcode.com/problems/" + str(m.group(1))])
