0121fce1a3c758fa907336df8d8f7201eb753ed9
Goh Jet Wei <goh_jetwei@mymail.sutd.edu.sg>
2025-05-02 02:11:22 +0800

    Update readme_generator.py

--- readme_generator.py
+++ readme_generator.py
@@ -8,7 +8,7 @@ upload a new solution to a Leetcode question in this repository!
 My Leetcode account: https://leetcode.com/WindJammer6/
 
 <ins>Disclaimer</ins>: I am a student programmer, these solutions that I have attached in this repository are not perfect answers to the Leetcode questions (in terms of design 
-and Time and Space Big O Notation Complexity), but are just merely my sharing of my own approach to the questions (in terms of design and Time and Space Big O Notation Complexity).
+and Time and Space Big O Notation Complexity), but are just merely my sharing of my own approach to the questions.
 They do answer the Leetcode questions, but there are definitely better answer codes out there.
 
 # List of Leetcode Questions Solved
