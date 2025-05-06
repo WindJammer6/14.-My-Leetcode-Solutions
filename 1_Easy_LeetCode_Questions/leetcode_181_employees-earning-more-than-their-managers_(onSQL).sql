672838864a1faa4a17b0b8494c7b5d6f09ea2ad7
Goh Jet Wei <goh_jetwei@mymail.sutd.edu.sg>
2025-05-06 21:23:21 +0800

    Update leetcode_181_employees-earning-more-than-their-managers_(onSQL).sql

--- 1_Easy_LeetCode_Questions/leetcode_181_employees-earning-more-than-their-managers_(onSQL).sql
+++ 1_Easy_LeetCode_Questions/leetcode_181_employees-earning-more-than-their-managers_(onSQL).sql
@@ -1,8 +1,9 @@
 -- Using MySQL (SQL implementation)
 -- This question tests you on using aliases on MySQL and self inner joins of a table
--- About MySQL aliases"
+
+-- About MySQL aliases:
 -- Source(s):
--- https://www.w3schools.com/mysql/mysql_alias.asp (GeekforGeeks)
+-- https://www.w3schools.com/mysql/mysql_alias.asp (W3Schools)
 SELECT e.name
 AS 'Employee'
 FROM Employee e
